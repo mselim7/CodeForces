89215b68f595e015ace131ef8b96c36557986158
mahmoudselim07 <mahmoud.selim21@outlook.com>
2023-01-02 01:46:52 +0200

    Problem 4 solved

---
+++ 546A - Soldier and Bananas/Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int k, n, w,p=0;
+	cin>>k>>n>>w;
+	
+	for (int i = 1; i <= w; i++)
+	{
+		p += i * k;
+	}
+	if (n >= p)
+	{
+		cout << 0;
+	}
+	else {
+		cout << p - n;
+	}
+
+}
